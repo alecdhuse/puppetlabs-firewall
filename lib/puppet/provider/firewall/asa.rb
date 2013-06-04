3b49a4359617a17712d035166e241ffd83b9dbe5
Alec Dhuse <alecdhuse@gmail.com>
2013-06-04 10:00:33 -0700

    Cleaned up ASA Providor
    Cleaned up the skeleton for the ASA providor.

--- lib/puppet/provider/firewall/asa.rb
+++ lib/puppet/provider/firewall/asa.rb
@@ -8,10 +8,6 @@ Puppet::Type.type(:firewall).provide :asa, :parent => Puppet::Provider::Firewall
 
   has_feature :log_level
 
-  optional_commands({
-
-  })
-
   defaultfor :ios => :asa
 
   asa_version = Facter.fact('asa_version').value
@@ -19,23 +15,12 @@ Puppet::Type.type(:firewall).provide :asa, :parent => Puppet::Provider::Firewall
   @protocol = "IPv4"
 
   @resource_map = {
-    :destination => "-d",
-    :dport => ["-m multiport --dports", "-m (udp|tcp) --dport"],
-    :icmp => "-m icmp --icmp-type",
-    :iniface => "-i",
-    :log_level => "--log-level",
-    :name => "-m comment --comment",
-    :outiface => "-o",
-    :port => '-m multiport --ports',
-    :proto => "-p",
-    :reject => "--reject-with",
-    :source => "-s",
-    :sport => ["-m multiport --sports", "-m (udp|tcp) --sport"],
-    :tcp_flags => "-m tcp --tcp-flags",
-    :todest => "--to-destination",
-    :toports => "--to-ports",
-    :tosource => "--to-source",
+    :log_level  => "log",
+    :port       => "eq",
+    :reject     => "deny",
+    :sport      => "eq",
   }
 
+  @resource_list = [:type, :access-list, :line, :extended, :action, :protocol, :source, :sport, :destination, :dport, :log]   
 
 end
